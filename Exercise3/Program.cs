9755e17b459a8b62dc27144bc8c08cec5ec539e7
Roqi SB <roqisb9@gmail.com>
2022-11-21 15:48:11 +0700

    add seacrh

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -9,4 +9,26 @@ namespace Exercise_Linked_List_D
         public string name;
         public Node next;
     }
+    class CircularList
+    {
+        Node LAST;
+
+        public CircularList()
+        {
+            LAST = null;
+        }
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        /*Searches for the specified node*/
+        {
+            for (previous = current = LAST.next; current != LAST; previous = current, current = current.next)
+            {
+                if (rollNo == current.rollNumber)
+                    return (true);/*return false if the node is not found*/
+            }
+            if (rollNo != LAST.rollNumber)
+                return true;
+            else
+                    return (false);
+        }
+    }
 }
