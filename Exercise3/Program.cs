1cd259a99aa3afe9f59c1e415d3f3d82dbf8f51e
Roqi SB <roqisb9@gmail.com>
2022-11-21 16:07:45 +0700

    add switch case

--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -78,7 +78,35 @@ namespace Exercise_Linked_List_D
                     Console.WriteLine("4. Exit");
                     Console.WriteLine("\nEnter your choice (1-4) ");
                     char ch = Convert.ToChar(Console.ReadLine());
-
+                    switch (ch)
+                    {
+                        case '1':
+                            {
+                                obj.traverse();
+                            }
+                            break;
+                        case '2':
+                            {
+                                if (obj.listEmpty() == true)
+                                {
+                                    Console.WriteLine("\nList is empty");
+                                    break;
+                                }
+                                Node prev, curr;
+                                prev = curr = null;
+                                Console.Write("\nEnter the roll number of the student whose record is to be searched: ");
+                                int num = Convert.ToInt32(Console.ReadLine());
+                                if (obj.Search(num, ref prev, ref curr) == false)
+                                    Console.WriteLine("\nRecord not found");
+                                else
+                                {
+                                    Console.WriteLine("\nRecord found");
+                                    Console.WriteLine("\nRoll number: " + curr.rollNumber);
+                                    Console.WriteLine("\nName: " + curr.name);
+                                }
+                            }
+                            break;
+                    }
                 }
             }
         }
